9d51fb0f9001d9f768359fac057293677dcfdc75
PhilaNgcamu <philangcamu17@gmail.com>
2023-01-17 10:51:41 +0200

    Using JSX as an expression

--- IntroducingJSX/intro.jsx
+++ IntroducingJSX/intro.jsx
@@ -12,3 +12,11 @@ const user = {
 };
 
 const h1Element = <h1>Hello, {formatName(user)}</h1>;
+
+//Using JSX as an expression
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
