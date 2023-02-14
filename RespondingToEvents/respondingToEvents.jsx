86db062c87432ea96f6f665fc802c9474595e287
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-14 10:01:09 +0200

    Responding to Events

--- RespondingToEvents/respondingToEvents.jsx
+++ RespondingToEvents/respondingToEvents.jsx
@@ -2,5 +2,5 @@ export default function Button() {
   function handleClick() {
     alert("You Clicked Me!");
   }
-  return <button onclick={handleClick}>Click Me!</button>;
+  return <button onClick={handleClick}>Click Me!</button>;
 }
