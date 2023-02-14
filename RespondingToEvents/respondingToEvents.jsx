b96cead5ab72a8d7289c4b528320b8fc99d63a46
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-14 10:45:50 +0200

    Responding to Events

--- RespondingToEvents/respondingToEvents.jsx
+++ RespondingToEvents/respondingToEvents.jsx
@@ -1,19 +1,19 @@
-export function Button() {
+export function AlertButton() {
   function handleClick() {
     alert("You Clicked Me!");
   }
   return <button onClick={handleClick}>Click Me!</button>;
 }
 
-function AlertButton({ message, children }) {
+function Button({ message, children }) {
   return <button onClick={() => alert(message)}>{children}</button>;
 }
 
 export default function ToolBar() {
   return (
     <>
-      <AlertButton message="Playing Music!">Play Music</AlertButton>
-      <AlertButton message="Playing Movie!">Play Movie</AlertButton>
+      <Button message="Playing Music!">Play Music</Button>
+      <Button message="Playing Movie!">Play Movie</Button>
     </>
   );
 }
