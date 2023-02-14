f277332cc83b18d6cedf18b07b846f80680e3bb2
PhilaNgcamu <philangcamu17@gmail.com>
2023-02-14 10:21:46 +0200

    Responding to Events

--- RespondingToEvents/respondingToEvents.jsx
+++ RespondingToEvents/respondingToEvents.jsx
@@ -1,6 +1,14 @@
-export default function Button() {
+import { Children } from "react";
+
+export function Button() {
   function handleClick() {
     alert("You Clicked Me!");
   }
   return <button onClick={handleClick}>Click Me!</button>;
 }
+
+function AlertButton({ message, children }) {
+  return <button onClick={() => alert(message)}>{children}</button>;
+}
+
+export default function ToolBar() {}
