70a6631a8e2aa3220bfbbc379415c5907a2265d9
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-23 08:50:11 +0200

    Strings In JSX

--- DescribingTheUI/stringInJsx.js
+++ DescribingTheUI/stringInJsx.js
@@ -1,9 +1,6 @@
 export default function Avatar() {
-  return (
-    <img
-      className="avatar"
-      src="https://i.imgur.com/7vQD0fPs.jpg"
-      alt="Gregorio Y. Zara"
-    />
-  );
+  const className = "avatar";
+  const avatar = "https://i.imgur.com/7vQD0fPs.jpg";
+  const description = "Gregorio Y. Zara";
+  return <img className={className} src={avatar} alt={description} />;
 }
