51721ff601b00206e5fb5bf5170990f8d4f6e86b
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-23 09:20:17 +0200

    Strings In JSX

--- DescribingTheUI/stringInJsx.js
+++ DescribingTheUI/stringInJsx.js
@@ -4,3 +4,8 @@ export default function Avatar() {
   const description = "Gregorio Y. Zara";
   return <img className={className} src={avatar} alt={description} />;
 }
+
+export function TodoList() {
+  const name = "Philasande Ngcamu";
+  return <h1>{name}'s To Do List</h1>;
+}
