f376d491a75a932a056e86de6ffee7f1bf55b95c
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-23 09:26:39 +0200

    JavaScript in JSX

--- DescribingTheUI/stringInJsx.js
+++ DescribingTheUI/stringInJsx.js
@@ -5,7 +5,13 @@ export default function Avatar() {
   return <img className={className} src={avatar} alt={description} />;
 }
 
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+}
+
 export function TodoList() {
   const name = "Philasande Ngcamu";
-  return <h1>{name}'s To Do List</h1>;
+  return <h1>{formatDate(today)}'s To Do List</h1>;
 }
