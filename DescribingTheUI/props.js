f738c4ec5286b536d4f6ed3c4c1937da428ab197
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-24 08:28:34 +0200

    Passing props to a component

--- DescribingTheUI/props.js
+++ DescribingTheUI/props.js
@@ -2,7 +2,7 @@ function Avatar({ person, size }) {
   return (
     <img
       classname="avatar"
-      src="https://i.imgur.com/1bX5QH6.jpg"
+      src={getImageUrl(person)}
       alt={person.name}
       width={size}
       height={size}
@@ -10,11 +10,25 @@ function Avatar({ person, size }) {
   );
 }
 
+function getImageUrl(person, size = "s") {
+  return "https://i.imgur.com/" + person.imageId + size + ".jpg";
+}
+
 export default function Profile() {
   return (
-    <Avatar
-      person={{ name: "Philasande Ngcamu", imageId: "1bX5QH6" }}
-      size={100}
-    />
+    <>
+      <Avatar
+        person={{ name: "Philasande Ngcamu", imageId: "1bX5QH6" }}
+        size={100}
+      />
+      <Avatar
+        person={{ name: "Katsuko Saruhashi", imageId: "YfeOqp2" }}
+        size={100}
+      />
+      <Avatar
+        person={{ name: "Aklilu Lemma", imageId: "OKS67lh" }}
+        size={100}
+      />
+    </>
   );
 }
