86be47af4ee3eeb3c4cd85c171a0d8d98dc595d6
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-27 10:32:01 +0200

    Conditional Rendering

---
+++ DescribingTheUI/conditionalRendering.js
@@ -0,0 +1,14 @@
+function Item({ name, isPacked }) {
+  if (isPacked) return <li className="item">{name}</li>;
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item isPacked={true}>Space suit</Item>
+      </ul>
+    </section>
+  );
+}
