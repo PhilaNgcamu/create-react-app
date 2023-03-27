5872053fbecb896a3dc38e60eaf9bb447664c71d
PhilaNgcamu <philangcamu17@gmail.com>
2023-03-27 10:41:02 +0200

    Conditional Rendering

--- DescribingTheUI/conditionalRendering.js
+++ DescribingTheUI/conditionalRendering.js
@@ -8,6 +8,8 @@ export default function PackingList() {
       <h1>Sally Ride's Packing List</h1>
       <ul>
         <Item isPacked={true}>Space suit</Item>
+        <Item isPacked={true}>Helmet with a golden leaf</Item>
+        <Item isPacked={true}>Photo of Tam</Item>
       </ul>
     </section>
   );
