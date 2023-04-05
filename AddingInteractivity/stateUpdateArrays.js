c96a9aaf81d63eed963487d722187223d17b1d37
PhilaNgcamu <philangcamu17@gmail.com>
2023-04-05 15:50:09 +0200

    Adding Interactivity

--- AddingInteractivity/stateUpdateArrays.js
+++ AddingInteractivity/stateUpdateArrays.js
@@ -1,31 +1,38 @@
 import { useState } from "react";
 
-let nextId = 0;
-
 export default function List() {
-  const [name, setName] = useState("");
-  const [artists, setArtists] = useState([]);
-
   let initialArtists = [
     { id: 0, name: "Marta Colvin Andrade" },
     { id: 1, name: "Lamidi Olonade Fakeye" },
     { id: 2, name: "Louise Nevelson" },
   ];
 
+  const [artists, setArtists] = useState(initialArtists);
+
   return (
     <>
-      <h1>Inspiring sculptors:</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button
-        onClick={() => {
-          setArtists([{ id: nextId++, name: name }, ...artists]);
-        }}
-      >
-        Add
-      </button>
+      <h1>Inspiring Sculptors:</h1>
       <ul>
         {artists.map((artist) => {
-          return <li key={artist.id}>{artist.name}</li>;
+          return (
+            <>
+              <li key={artist.id}>
+                {artist.name}{" "}
+                <button
+                  onClick={() => {
+                    setArtists(
+                      artists.filter((art) => {
+                        console.log(art.id, artist.id);
+                        return art.id !== artist.id;
+                      })
+                    );
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            </>
+          );
         })}
       </ul>
     </>
