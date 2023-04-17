600620efccef448d71110613bcebfaac02cd6e04
PhilaNgcamu <philangcamu17@gmail.com>
2023-04-17 11:45:20 +0200

    Managing State

--- ManagingState/stateExamples.js
+++ ManagingState/stateExamples.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const obj = { count: 0, color: "red" };
+  const [count, setCount] = useState(obj);
   function decrementCount() {
     setCount((c) => c - 1);
   }
@@ -11,8 +12,7 @@ function App() {
   return (
     <>
       <button onClick={decrementCount}> - </button>
-      <span> {count} </span>
-      <button onClick={incrementCount}> + </button>
+      <span> {count} </span> a<button onClick={incrementCount}> + </button>
     </>
   );
 }
